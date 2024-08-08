c613cf1bcb0acfe2c468405478167a27b776361b
meba-yonas <mebayonasyonas@gmail.com>
2024-08-08 21:35:15 +0300

    header

---
+++ components/header/script.js
@@ -0,0 +1,4 @@
+document.querySelector(".hamburger").addEventListener("click", function () {
+	var responsiveMenu = document.querySelector(".responsive-menu");
+	responsiveMenu.classList.toggle("show");
+});
